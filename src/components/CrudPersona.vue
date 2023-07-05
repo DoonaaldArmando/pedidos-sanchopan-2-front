e7025efe2d05b4213fb35805e1abdbf04647e1d0
Sergio <elsercaser@gmail.com>
2023-07-04 23:11:13 -0500

    Commit inicial

---
+++ src/components/CrudPersona.vue
@@ -0,0 +1,7 @@
+<template>
+   <div>
+      <h1>Persona</h1>
+      <v-btn color="success">Nueva persona</v-btn>
+      
+   </div>
+</template>
---
+++ src/components/PanelIzquierdo.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+      <v-navigation-drawer v-model="drawer">
+         <!--  -->
+      </v-navigation-drawer>
+   </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+</script>
+ 
+<script>
+export default {
+   data: () => ({ drawer: null }),
+}
+</script>
