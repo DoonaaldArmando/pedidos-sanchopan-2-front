e688200a2eebf212c4c74e78964ce79dbaf699d4
Sergio <elsercaser@gmail.com>
2023-07-04 23:40:41 -0500

    wip: formulario de crear persona

--- src/router/index.js
+++ src/router/index.js
@@ -9,10 +9,12 @@ const routes = [
       {
         path: '',
         name: 'Inicio',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Inicio.vue'),
+      },
+      {
+        path: '/crearpersona',
+        name: 'CrearPersona',
+        component: () => import(/* webpackChunkName: "home" */ '@/views/CrearPersona.vue'),
       },
     ],
   },
