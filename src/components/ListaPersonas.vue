f39e18331b37a0fd9d8ea6c7877347de1c3581d0
Sergio <elsercaser@gmail.com>
2023-07-05 23:38:00 -0500

    wip: formulario nueva persona terminado en funcionalidad.

---
+++ src/components/ListaPersonas.vue
@@ -0,0 +1,165 @@
+<template>
+   <div>
+      <v-table>
+         <thead>
+            <tr>
+               <th class="text-left">
+               Documento
+               </th>
+               <th class="text-left">
+               Nombres
+               </th>
+               <th class="text-left">
+               Apellidos
+               </th>
+               <th class="text-left">
+               Teléfono
+               </th>
+            </tr>
+         </thead>
+         <tbody>
+            <tr
+               v-for="item in personas"
+               :key="item.numeroDocumento"
+            >
+               <td>{{ item.numeroDocumento }}</td>
+               <td>{{ item.primerNombre +" "+ item.segundoNombre }}</td>
+               <td>{{ item.apellidos }}</td>
+               <td>{{ item.telefono }}</td>
+            </tr>
+         </tbody>
+      </v-table>
+   </div>
+</template>
+
+<script>
+export default {
+   data() {
+      return {
+         itemsPorPagina: 7,
+         personas: [
+            {
+               numeroDocumento: "2111100000",
+               tipoDocumento: "Cedula de ciudadanía",
+               primerNombre: "Andrés",
+               segundoNombre: "Felipe",
+               apellidos: "Herrera Gómez",
+               direccion: "Carrera 7 #15-30",
+               municipio: "Monteria",
+               departamento: "Cordoba",
+               telefono: "3151234567",
+               email: "andres.herrera@gmail.com"
+            },
+            {
+               numeroDocumento: "2111100001",
+               tipoDocumento: "Cedula de ciudadanía",
+               primerNombre: "María",
+               segundoNombre: "Alejandra",
+               apellidos: "López Pérez",
+               direccion: "Avenida 10 #25-15",
+               municipio: "Bogotá",
+               departamento: "Cundinamarca",
+               telefono: "3209876543",
+               email: "maria.lopez@gmail.com"
+            },
+            {
+               numeroDocumento: "2111100002",
+               tipoDocumento: "Cedula de extranjería",
+               primerNombre: "Juan",
+               segundoNombre: "Carlos",
+               apellidos: "Ramírez Sánchez",
+               direccion: "Calle 50 #8-45",
+               municipio: "Medellín",
+               departamento: "Antioquia",
+               telefono: "3105432167",
+               email: "juan.ramirez@gmail.com"
+            },
+            {
+               numeroDocumento: "2111100003",
+               tipoDocumento: "Pasaporte",
+               primerNombre: "Laura",
+               segundoNombre: "Fernanda",
+               apellidos: "García Mendoza",
+               direccion: "Carrera 15 #40-12",
+               municipio: "Cali",
+               departamento: "Valle del Cauca",
+               telefono: "3177896543",
+               email: "laura.fernanda@gmail.com"
+            },
+            {
+               numeroDocumento: "2111100004",
+               tipoDocumento: "Tarjeta de identidad",
+               primerNombre: "Pedro",
+               segundoNombre: "Andrés",
+               apellidos: "Londoño Vargas",
+               direccion: "Avenida 5 #12-30",
+               municipio: "Pereira",
+               departamento: "Risaralda",
+               telefono: "3006549871",
+               email: "pedro.londono@example.com"
+            },
+            {
+               numeroDocumento: "2111100005",
+               tipoDocumento: "Cedula de ciudadanía",
+               primerNombre: "Ana",
+               segundoNombre: "María",
+               apellidos: "González López",
+               direccion: "Calle 12 #18-22",
+               municipio: "Barranquilla",
+               departamento: "Atlántico",
+               telefono: "3012345678",
+               email: "ana.gonzalez@example.com"
+            },
+            {
+               numeroDocumento: "2111100006",
+               tipoDocumento: "Cedula de extranjería",
+               primerNombre: "Eduardo",
+               segundoNombre: "Antonio",
+               apellidos: "Silva Gutiérrez",
+               direccion: "Avenida 20 #30-10",
+               municipio: "Cartagena",
+               departamento: "Bolívar",
+               telefono: "3109876543",
+               email: "eduardo.silva@example.com"
+            },
+            {
+               numeroDocumento: "2111100007",
+               tipoDocumento: "Pasaporte",
+               primerNombre: "Carolina",
+               segundoNombre: "Isabel",
+               apellidos: "Castro Morales",
+               direccion: "Carrera 25 #15-50",
+               municipio: "Bucaramanga",
+               departamento: "Santander",
+               telefono: "3178765432",
+               email: "carolina.castro@example.com"
+            },
+            {
+               numeroDocumento: "2111100008",
+               tipoDocumento: "Tarjeta de identidad",
+               primerNombre: "Gabriel",
+               segundoNombre: "Andrés",
+               apellidos: "López Torres",
+               direccion: "Calle 5 #10-35",
+               municipio: "Ibagué",
+               departamento: "Tolima",
+               telefono: "3223456789",
+               email: "gabriel.lopez@example.com"
+            },
+            {
+               numeroDocumento: "2111100009",
+               tipoDocumento: "Cedula de ciudadanía",
+               primerNombre: "Valentina",
+               segundoNombre: "Fernández",
+               apellidos: "Hernández Gómez",
+               direccion: "Carrera 18 #22-15",
+               municipio: "Santa Marta",
+               departamento: "Magdalena",
+               telefono: "3156789012",
+               email: "valentina.fernandez@example.com"
+            }
+         ]
+      }
+   }
+}
+</script>
--- src/views/CrearPersona.vue
+++ src/views/CrearPersona.vue
@@ -2,54 +2,294 @@
    <v-form v-model="valid">
       <v-container>
          <v-row>
+            <v-col cols="12">
+               <h1>Nueva persona</h1>
+            </v-col>
+            
             <v-col cols="12" md="4">
-               <v-text-field v-model="firstname" :rules="nameRules" :counter="10" label="First name"
-                  required></v-text-field>
+               <v-select
+                  :rules="reglasTipoDocumento"
+                  label="Tipo de documento"
+                  :items="tiposDocumento"
+                  required
+               ></v-select>
             </v-col>
 
             <v-col cols="12" md="4">
-               <v-text-field v-model="lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field>
+               <v-text-field 
+                  v-model="numeroDocumento" 
+                  :rules="reglasNumeroDocumento" 
+                  :counter="11" 
+                  label="Número de documento"
+                  required>
+               </v-text-field>
             </v-col>
 
             <v-col cols="12" md="4">
-               <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+               <v-text-field 
+                  v-model="primerNombre" 
+                  :rules="reglasPrimerNombre" 
+                  :counter="20" 
+                  label="Primer nombre"
+                  required>
+               </v-text-field>
             </v-col>
+
+            <v-col cols="12" md="4">
+               <v-text-field 
+                  v-model="segundoNombre" 
+                  :rules="reglasSegundoNombre" 
+                  :counter="20" 
+                  label="Segundo nombre">
+               </v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+               <v-text-field 
+                  v-model="apellidos" 
+                  :rules="reglasApellidos" 
+                  :counter="40" 
+                  label="Apellidos" 
+                  required>
+               </v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+               <v-text-field 
+                  v-model="direccion" 
+                  :rules="reglasDireccion" 
+                  :counter="60" 
+                  label="Dirección de residencía" 
+                  required>
+               </v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+               <v-autocomplete
+                  :rules="reglasDepartamento"
+                  v-model="departamento"
+                  label="Departamento"
+                  :items="departamentos"
+                  item-value="codigoDepartamento"
+                  item-title="nombreDepartamento"
+                  @update:model-value="leerMunicipios"
+                  required
+               ></v-autocomplete>
+            </v-col>
+
+            <v-col cols="12" md="4">
+               <v-autocomplete
+                  :rules="reglasMunicipio"
+                  label="Municipio"
+                  :items="municipios"
+                  item-value="codigoMunicipio"
+                  item-title="nombreMunicipio"
+                  required
+               ></v-autocomplete>
+            </v-col>
+
+            <v-col cols="12" md="4">
+               <v-text-field v-model="email" label="E-mail"></v-text-field>
+            </v-col>
+
+            <v-col cols="12" md="4">
+               <v-text-field 
+               v-model="telefono" 
+               :rules="reglasTelefono" 
+                  :counter="20" 
+                  label="Teléfono">
+               </v-text-field>
+            </v-col>
+            
+            <v-col cols="12">
+               <v-btn type="submit" class="mt-2">Enviar</v-btn>
+            </v-col>
+
          </v-row>
       </v-container>
    </v-form>
 </template>
 
 <script>
+/*
+numeroDocumento: "2111100006",
+tipoDocumento: "Cedula de extranjería",
+primerNombre: "Eduardo",
+segundoNombre: "Antonio",
+apellidos: "Silva Gutiérrez",
+direccion: "Avenida 20 #30-10",
+municipio: "Cartagena",
+departamento: "Bolívar",
+telefono: "3109876543"
+email
+*/
 export default {
+   mounted() {
+      this.leerDepartamentos();
+   },
    data: () => ({
       valid: false,
-      firstname: '',
-      lastname: '',
-      nameRules: [
+      numeroDocumento: '',
+      tipoDocumento: '',
+      primerNombre: '',
+      segundoNombre: '',
+      apellidos: '',
+      direccion: '',
+      municipio: null,
+      departamento: null,
+      telefono: '',
+      email: '',
+      departamentos: [],
+      municipios: [],
+      tiposDocumento: [
+         'Cédula de ciudadanía',
+         'Cédula de extranjería',
+         'Pasaporte'
+      ],
+      reglasTipoDocumento: [
          value => {
             if (value) return true
 
-            return 'Name is required.'
-         },
-         value => {
-            if (value?.length <= 10) return true
-
-            return 'Name must be less than 10 characters.'
+            return 'El tipo de documento es obligatorio.'
          },
       ],
-      email: '',
-      emailRules: [
+      reglasNumeroDocumento: [
          value => {
             if (value) return true
 
-            return 'E-mail is requred.'
+            return 'El número de documento es obligatorio.'
          },
+         value => {
+            if (value?.length <= 11) return true
+
+            return 'El número de documento debe contener menos de 11 caracteres.'
+         },
+      ],
+      reglasPrimerNombre: [
+         value => {
+            if (value) return true
+
+            return 'El primer nombre es obligatorio.'
+         },
+         value => {
+            if (value?.length <= 20) return true
+
+            return 'El nombre debe tener menos de 20 caracteres.'
+         },
+         value => {
+            if (/[^0-9]/.test(value)) return true
+
+            return 'El nombre no puede contener números.'
+         },
+      ],
+      reglasSegundoNombre: [
+         value => {
+            if (value?.length <= 20) return true
+
+            return 'El nombre debe tener menos de 20 caracteres.'
+         },
+         value => {
+            if (/[^0-9]/.test(value)) return true
+
+            return 'El nombre no puede contener números.'
+         },
+      ],
+      reglasApellidos: [
+         value => {
+            if (value) return true
+
+            return 'El apellido es obligatorio.'
+         },
+         value => {
+            if (value?.length <= 40) return true
+
+            return 'El nombre debe tener menos de 40 caracteres.'
+         },
+         value => {
+            if (/[^0-9]/.test(value)) return true
+
+            return 'Los apellidos no pueden contener números.'
+         },
+      ],
+      reglasDireccion: [
+         value => {
+            if (value) return true
+
+            return 'La dirección es obligatoria.'
+         },
+         value => {
+            if (value?.length <= 60) return true
+
+            return 'La dirección debe contener menos de 60 caracteres.'
+         },
+      ],
+      reglasDepartamento: [
+         value => {
+            if (value) return true
+
+            return 'El departamento es obligatorio.'
+         },
+      ],
+      reglasMunicipio: [
+         value => {
+            if (value) return true
+
+            return 'El municipio es obligatorio.'
+         },
+      ],
+      reglasTelefono: [
+         value => {
+            if (value) return true
+
+            return 'El telefono es obligatorio.'
+         },
+         value => {
+            if (value?.length <= 12) return true
+
+            return 'El teléfono debe tener menos de 12 dígitos.'
+         },
+         value => {
+            if (/^\d+$/.test(value)) return true;
+
+            return 'El valor debe contener solo números.';
+         },
+      ],
+      reglasEmail: [
          value => {
             if (/.+@.+\..+/.test(value)) return true
 
-            return 'E-mail must be valid.'
+            return 'Debe ingresar una dirección de correo válida.'
          },
       ],
    }),
+   methods: {
+      leerDepartamentos() {
+         fetch("../src/assets/departamentos.json") // Ruta relativa al archivo JSON
+            .then(response => response.json())
+            .then(data => {
+               this.departamentos = data;
+               // Hacer cualquier otra operación con los datos JSON aquí
+            })
+            .catch(error => {
+               console.error("Error al leer el archivo JSON:", error);
+            });
+      },
+      leerMunicipios(departamento) {
+         fetch("../src/assets/municipios.json") // Ruta relativa al archivo JSON
+            .then(response => response.json())
+            .then(data => {
+               var municipios = data;
+               this.municipios = municipios.filter(
+                  function(municipio){
+                     return municipio.codigoDepartamento == departamento;
+                  }
+               )
+               // Hacer cualquier otra operación con los datos JSON aquí
+            })
+            .catch(error => {
+               console.error("Error al leer el archivo JSON:", error);
+            });
+      }
+   }
 }
 </script>
